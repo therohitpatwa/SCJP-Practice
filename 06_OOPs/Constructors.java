7bd16bcadaef082c03d0e0a4060ed49a4de8d150
Rohit Patwa <therohitpatwa@gmail.com>
2025-07-13 15:35:45 +0530

    Day5

---
+++ 06_OOPs/Constructors.java
@@ -0,0 +1,67 @@
+class Student
+{
+    String name;
+    int roll;
+
+    Student(String name,int roll) {
+        this.name=name;
+        this.roll=roll;
+    }
+    public static void main(String[] args) {
+        Student s=new Student("Rohit", 101);
+    }
+    
+}
+
+
+
+
+// class  Student
+// {
+//     void Student()
+//     {
+
+//     }
+// }
+
+
+
+
+
+// class Student{
+//     static Student()
+//     {
+
+//     }
+// }
+
+
+
+// class Test
+// {
+//     Test(double d)
+//     {
+//         this(10);
+//         System.out.println("Double-argument constructor");
+//     }
+//     Test(int i)
+//     {
+//         this();
+//         System.out.println("int-arg constructor");
+//     }
+//     Test()
+//     {
+//         System.out.println("no-arg constructor");
+//     }
+//     public static void main(String[] args) {
+//         Test t1=new Test(10.5);
+//         Test t2=new Test(10);
+//         Test t3=new Test();
+//         Test t4=new Test();
+
+//     }
+// }
+
+
+
+
---
+++ 06_OOPs/SingletonClass.java
@@ -0,0 +1,27 @@
+
+class Test {
+
+    private static Test t = null;
+
+    private Test() {
+
+    }
+
+    public static Test getTest() {
+        if (t == null) {
+            t = new Test();
+
+        }
+        return t;
+    }
+}
+
+class Client {
+
+    public static void main(String[] args) {
+        System.out.println(Test.getTest().hashCode());
+        System.out.println(Test.getTest().hashCode());
+        System.out.println(Test.getTest().hashCode());
+        System.out.println(Test.getTest().hashCode());
+    }
+}
